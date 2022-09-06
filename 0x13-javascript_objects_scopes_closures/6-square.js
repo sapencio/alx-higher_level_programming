85558a0f2ffb819ceee8400adf43a8a17077ad9c
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-09-06 16:04:40 +0300

    A class Square that defines a square and inherits from Square class.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-      super.print();
+      this.print();
     } else {
-      for (let i = 0; i < this.height; i++) console.log(C.repeat(this.width));
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
