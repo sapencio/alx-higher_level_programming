465f799c7f6998069b4c9a0efdbc841fbf1405c4
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-09-06 16:33:36 +0300

    A script that imports a dictionary of occurrences by user id an computes a dictionary of user ids by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
 }
-console.log(newDict);
+console.log(Dictn);
