2ab2b2602e9f6d376e3b79fb83f63fb643bd295c
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-09-11 01:27:15 +0300

    A script that lists all City objects from the database.

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -17,3 +17,5 @@ if __name__ == "__main__":
 
     for city in session.query(City).order_by(City.id):
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
