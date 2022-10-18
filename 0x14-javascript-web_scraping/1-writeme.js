ce7d4732b9f05e246f435d865f040d398fbfa5a5
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-10-18 16:00:18 +0300

    A script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const at = require('at');
-at.writeFile(process.argv[2], process.argv[3], error => {
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
 });
